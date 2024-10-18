2aff6dfbc5a53e731918130d29afe7368429791a
wangyongwang <yongwang.wang@stepai.cn>
2024-10-18 13:02:34 +0800

    add dev

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 
 
+double dev(double x, double y) {
+    return x/y;
+}
 
 int main() {    
     printf("Hello, World!\n");
+
+    int a = 5;
+    int b = 2;
+    double c = dev(a,b);
+    printf("%f\n", c);
 }
 
 
